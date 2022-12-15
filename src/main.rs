cd29e0f3a2d7024669aa33eee158b02c350f1a83
Manank Patel <pmanank200502@gmail.com>
2022-12-15 05:51:07 +0530

    able to store in file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 #[allow(unused_imports)]
-use std::net::{SocketAddr, TcpListener, TcpStream};
-use avro_rs::{Reader, from_avro_datum, Schema};
+use std::net::{ TcpListener, TcpStream};
+use avro_rs::{Reader, Schema};
+use chrono::format::StrftimeItems;
+use std::fs::{File, self};
+use std::thread;
+use crossbeam_queue::SegQueue;
+use chrono::{Duration, Days, prelude::*};
+use tokio::time::sleep;
+use std::io::prelude::*;
+// use std::io::Write; use std::sync::Arc;
 
-fn main() {
-    
-    let raw_schema = r#"
+
+const RAW_SCHEMA: &str = r#"
         {
             "type": "record",
             "namespace": "com.geofen",
@@ -18,25 +25,67 @@ fn main() {
             ]
         }
     "#;
-    let schema = Schema::parse_str(raw_schema).unwrap();
 
-    println!("Hello, DSTN!");
-    let mut socket = TcpStream::connect("localhost:8000").unwrap();
+const IP_ADDRESS: &str = "localhost:8000";
+
+
+#[tokio::main]
+async fn main() -> ! {
+
+    let schema = Schema::parse_str(RAW_SCHEMA).unwrap();
+
+    let mut socket = TcpStream::connect(IP_ADDRESS).unwrap();
     println!("Connected to server");
-    // let socket = socket;
 
-    // let reader = Reader::new(&mut socket).unwrap();
+    let avro_queue = SegQueue::new();
+
+    let mut filename = init_file();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            loop{
+                if let Some(x) = avro_queue.pop(){
+                    let x = format!("{:?}\n", x);
+                    println!("Writing to file {:?} : {}",filename, x);
+                    filename.write_all(x.as_bytes()).unwrap();
+                    println!("{:?}",x);
+                }
+            }
+        });
+
+        s.spawn(|| {
+            let mut reader = Reader::with_schema(&schema, &mut socket).unwrap();
+            loop{
+                for result in &mut reader {
+                    let record = result.unwrap();
+                    avro_queue.push(record);
+                }
+            }
+        });
+    });
+
+    // loop{
+    //     thread::sleep(Duration::seconds(10).to_std().unwrap());
+    //     //compress files and create new for next day
+    //     filed = init_file();
+    // }
+    loop{}
+
     
-    loop {
-        println!("Waiting for data");
-        let record = Reader::with_schema(&schema, &mut socket).unwrap();/* from_avro_datum(&schema,result, None); */
-        for result in record {
-            println!("{:?}", result);
-        }
-    }
-    /* for value in record {
-        println!("{:?}", value);
-    } */
-    // println!("{:?}", record);
+    // loop {
+    //     println!("Waiting for data");
+    //     let record = Reader::with_schema(&schema, &mut socket).unwrap();/* from_avro_datum(&schema,result, None); */
+    //     for result in record {
+    //         avro_queue.push_back(result.unwrap());
+    //     }
+    // }
     
+}
+
+fn init_file() -> File{
+    let bind = Local::now().to_string();
+    let path = std::path::Path::new(bind.as_str());
+    let mut file = File::create(path).unwrap();
+    file.write_all(b"timestamp,device_id,latitude,longitude,altitude\n").unwrap();
+    file
 }
